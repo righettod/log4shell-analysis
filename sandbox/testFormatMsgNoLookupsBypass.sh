08621abdae8e608ec5a8e777cd19d69385de5be2
drighetto <drighetto@excellium-services.com>
2021-12-18 21:28:47 +0100

    Add section about JNDI-Exploit-Kit

--- sandbox/testFormatMsgNoLookupsBypass.sh
+++ sandbox/testFormatMsgNoLookupsBypass.sh
@@ -5,9 +5,11 @@ echo "[+] TEST"
 while IFS= read -r line
 do
   mvn -q -D"test=Log4ShellExposureTestFormatMsgNoLookupsBypass" -D"log4j2.target.version=$line" -D"log4j2.formatMsgNoLookups=true" clean test 1>/dev/null
-  rc=$?
-  v=$(cat target/Log4ShellExposureTestMsgNoLookupsBypass.out | head -1)
-  echo ">>> $v - RC: $rc"
+  rc1=$?
+  mvn -q -D"test=Log4ShellExposureTestFormatMsgNoLookupsBypassWithPrintf" -D"log4j2.target.version=$line" -D"log4j2.formatMsgNoLookups=true" clean test 1>/dev/null
+  rc2=$?
+  rc=$(($rc1 + $rc2))
+  echo ">>> RC: $rc"
   if [ $rc -ne 0 ]
   then
     echo "<<< Version $line flag log4j2.formatMsgNoLookups NOT effective."
