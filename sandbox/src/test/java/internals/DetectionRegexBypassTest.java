ee6ebbc2796493d9ffe85e2051f6901bf53a945e
drighetto <drighetto@excellium-services.com>
2021-12-20 17:20:50 +0100

    Add a test case to verify the exposure to ReDOS

--- sandbox/src/test/java/internals/DetectionRegexBypassTest.java
+++ sandbox/src/test/java/internals/DetectionRegexBypassTest.java
@@ -17,6 +17,8 @@ import java.util.stream.Collectors;
  * The list of bypasses gathered is used as a source of payloads.
  *
  * @see "https://github.com/righettod/log4shell-analysis/blob/main/payloads/README.md"
+ * @see "https://issues.apache.org/jira/browse/LOG4J2-3230"
+ * @See "https://issues.apache.org/jira/browse/LOG4J2-3230?focusedCommentId=17461971&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-17461971"
  */
 public class DetectionRegexBypassTest {
     private static List<String> PAYLOADS;
@@ -37,6 +39,23 @@ public class DetectionRegexBypassTest {
             boolean matchFound = matcher.find();
             Assert.assertTrue("Regex cannot spot the pattern: '" + p + "'", matchFound);
         });
+    }
+
+    @Test(timeout = 60000L)
+    public void testExposureToReDOS() {
+        //Generate the huge payload: "${" + "${::-"*500000 + "$${::-j}" + "}"*500000 + "}"
+        //Source is the ticket "LOG4J2-3230" - The comment added in tag SEE
+        StringBuilder payload = new StringBuilder("${");
+        int occurrences = 500000;
+        payload.append("${::-".repeat(occurrences));
+        payload.append("$${::-j}");
+        payload.append("}".repeat(occurrences));
+        payload.append("}");
+        //Apply test
+        Matcher matcher = DETECTION_REGEX.matcher(payload);
+        boolean matchFound = matcher.find();
+        System.out.println("Regex not prone to ReDOS.");
+        Assert.assertTrue("Regex cannot spot the pattern: '" + payload + "'", matchFound);
     }
 
 }
